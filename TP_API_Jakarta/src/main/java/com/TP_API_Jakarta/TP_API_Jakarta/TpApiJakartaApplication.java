b81fc20549157ce2e05a6519a632687fb1c6e0bd
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2024-04-18 10:26:59 +0200

    Add files via upload

---
+++ TP_API_Jakarta/src/main/java/com/TP_API_Jakarta/TP_API_Jakarta/TpApiJakartaApplication.java
@@ -0,0 +1,13 @@
+package com.TP_API_Jakarta.TP_API_Jakarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpApiJakartaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpApiJakartaApplication.class, args);
+	}
+
+}
---
+++ TP_API_Jakarta/src/test/java/com/TP_API_Jakarta/TP_API_Jakarta/TpApiJakartaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TP_API_Jakarta.TP_API_Jakarta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApiJakartaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
