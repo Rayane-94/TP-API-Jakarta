5f11d94744dcce6154e4babf55d9b13b704d1bfd
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2024-04-19 16:20:32 +0200

    commit

---
+++ TP_API_Jakarta/src/main/java/com/TP_API_Jakarta/TP_API_Jakarta/service/UtilisateurService.java
@@ -0,0 +1,15 @@
+package com.TP_API_Jakarta.TP_API_Jakarta.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.TP_API_Jakarta.TP_API_Jakarta.repository.UtilisateurRepository;
+
+
+@Service
+public class UtilisateurService {
+	private final UtilisateurRepository Utilisateur;
+
+    @Autowired
+    public UtilisateurService
+
+}
